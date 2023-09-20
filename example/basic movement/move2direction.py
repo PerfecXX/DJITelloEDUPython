e2de21d26fafcb33a3ec75a57961db66902b32eb
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-09-20 23:45:26 +0700

    Move to specific direction

---
+++ example/basic movement/move2direction.py
@@ -0,0 +1,30 @@
+# Import Library
+from djitellopy import Tello
+
+# Tello Setup
+tello = Tello()
+
+# Takeoff
+tello.takeoff()
+
+# Move up 20 cm
+tello.move_up(20)
+
+# Move down 20 cm
+tello.move_down(20)
+
+# Move left 20 cm
+tello.move_left(20)
+
+# Move right 20 cm
+tello.move_right(20)
+
+# Move forward 20 cm
+tello.move_forward(20)
+
+# Move backward 20 cm
+tello.move_back(20)
+
+# Landing
+tello.land()
+
