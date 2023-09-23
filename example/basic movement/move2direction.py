856144ceb3cbbe25df4f1e2278712e878addeeec
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-09-23 13:41:28 +0700

    Move the drone in the x direction by y cm.

--- example/basic movement/move2direction.py
+++ example/basic movement/move2direction.py
@@ -2,30 +2,30 @@
 from djitellopy import Tello
 
 # Tello Setup
-tello = Tello()
-tello.connect()
+drone = Tello()
+drone.connect()
 
 # Takeoff
-tello.takeoff()
+drone.takeoff()
 
 # Move up 20 cm
-tello.move_up(20)
+drone.move_up(20)
 
 # Move down 20 cm
-tello.move_down(20)
+drone.move_down(20)
 
 # Move left 20 cm
-tello.move_left(20)
+drone.move_left(20)
 
 # Move right 20 cm
-tello.move_right(20)
+drone.move_right(20)
 
 # Move forward 20 cm
-tello.move_forward(20)
+drone.move_forward(20)
 
 # Move backward 20 cm
-tello.move_back(20)
+drone.move_back(20)
 
 # Landing
-tello.land()
+drone.land()
 
